c5257d09285ebe79cb02dc0743decb436d8c42d2
zeynepdilaytoptas <zezidel@gmail.com>
2025-12-16 14:56:37 +0100

    Three different modes using FLASK_ENV

--- flask_option.py
+++ flask_option.py
@@ -3,8 +3,15 @@
 import os
 
 def start():
-    """returns the right message"""
-    pass  # YOUR CODE HERE
+    env = os.getenv("FLASK_ENV")
+
+    if env == "development":
+        return "Starting in development mode..."
+    elif env == "production":
+        return "Starting in production mode..."
+    else:
+        return "Starting in empty mode..."
+
 
 if __name__ == "__main__":
     print(start())
